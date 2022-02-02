850be5f29b55ed45f88e4e7c5fbeb92f6252a0ef
Marketa Sauerova <market.sauer@gmail.com>
2022-02-02 08:20:08 +0100

    Try to establish socket.io connection
    fails on error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.css';
 import { BrowserWindow } from './components/BrowserWindow';
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "http://localhost:8080";
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT, { transports: ["websocket"], rejectUnauthorized: false });
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <BrowserWindow></BrowserWindow>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          It's <time dateTime={response}>{response}</time>
         </p>
         <a
           className="App-link"
--- server.ts
+++ server.ts
@@ -5,10 +5,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import http from 'http';
+import cors from 'cors';
 
-import { Server } from "socket.io";
+import {Server} from "socket.io";
 
 const app = express();
+app.use(cors());
 // integrate socket.io for websocket communication
 // necesary for socket.io
 const server = http.createServer(app);
@@ -20,15 +22,28 @@ app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+let interval: any;
+
+io.on("connection", (socket : any) => {
+    console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
     });
 });
+
+const getApiAndEmit = (socket: any) => {
+    const response = new Date();
+    // Emitting a new message. Will be consumed by the client
+    socket.emit("FromAPI", response);
+};
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(8080, () => console.log(`Listening on port 8080`));
