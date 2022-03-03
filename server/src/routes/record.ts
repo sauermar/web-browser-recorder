988526e478900d102d41d8e79c8e7a5e69bdd6f9
Marketa Sauerova <market.sauer@gmail.com>
2022-03-03 21:58:06 +0100

    [BE] Add record API endpoints

--- server/src/routes/record.ts
+++ server/src/routes/record.ts
@@ -2,13 +2,27 @@ import { Router } from 'express';
 
 import { createRemoteBrowser, destroyRemoteBrowser } from '../browser-management/controller'
 import { chromium } from "playwright";
+import logger from "../logger";
 
 const router = Router();
+
+router.all('/', (req, res, next) => {
+    logger.log('debug',`The record API was invoked: ${req.url}`)
+    next() // pass control to the next handler
+})
 
 router.get('/start', (req, res) => {
     const id = createRemoteBrowser({
         browser: chromium,
         launchOptions: { headless: false }
+    });
+    return res.send(id);
+});
+
+router.post('/start', (req, res) => {
+    const id = createRemoteBrowser({
+        browser: chromium,
+        launchOptions: req.body,
     });
     return res.send(id);
 });
